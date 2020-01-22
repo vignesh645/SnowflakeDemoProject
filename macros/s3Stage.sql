af046a0c4b39ab64b12886a4613b4be104c471b9
vignesh645 <vigneshh@visualbi.com>
2020-01-22 16:50:41 -0600

    TestCommit

--- macros/s3Stage.sql
+++ macros/s3Stage.sql
@@ -2,7 +2,7 @@
 
   {% set sql %}
     create or replace stage my_s3_stage url='s3://snowflakeproject/Sales.csv'
-    credentials=(aws_key_id='XXXX' aws_secret_key='XXXX')
+    credentials=(aws_key_id='YYYY' aws_secret_key='XXXX')
     file_format =(type='CSV');
   {% endset %}
 
